9bfc62bda6616312551272219f8f45ebb497145e
isakya <1016361268@qq.com>
2024-08-18 23:24:33 +0800

    查询获取指定多个类别中的最新帖子、新建展示帖子的模板

--- page-home.php
+++ page-home.php
@@ -1,14 +1,31 @@
 <?php get_header(); ?>
 
 <?php
-$lastBlog = new WP_Query('type=post&posts_per_page=1');
-if ($lastBlog->have_posts()):
-    while ($lastBlog->have_posts()): $lastBlog->the_post(); ?>
-        <?php get_template_part('content', get_post_format()); ?>
-    <?php endwhile;
-endif;
 
-wp_reset_postdata();
+$args_cat = array(
+    'include' => '7,5,6'
+);
+
+$categories = get_categories($args_cat);
+foreach ($categories as $category):
+    $args = array(
+        'type' => 'post',
+        'posts_per_page' => 1,
+        'category__in' => $category->term_id,
+        'category__not_in' => array(1),
+    );
+
+    $lastBlog = new WP_Query($args);
+
+
+    if ($lastBlog->have_posts()):
+        while ($lastBlog->have_posts()): $lastBlog->the_post(); ?>
+            <?php get_template_part('content', 'featured'); ?>
+        <?php endwhile;
+    endif;
+
+    wp_reset_postdata();
+endforeach;
 ?>
 
     <hr/>
@@ -20,6 +37,7 @@ if (have_posts()):
     <?php endwhile;
 endif;
 
+/*
 $args = array(
     'type' => 'post',
     'posts_per_page' => 2,
@@ -33,11 +51,13 @@ if ($lastBlog->have_posts()):
 endif;
 
 wp_reset_postdata();
+*/
 ?>
 
-    <hr/>
+    <!--    <hr/>-->
 
 <?php
+/*
 $lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=news');
 if ($lastBlog->have_posts()):
     while ($lastBlog->have_posts()): $lastBlog->the_post(); ?>
@@ -46,6 +66,7 @@ if ($lastBlog->have_posts()):
 endif;
 
 wp_reset_postdata();
+*/
 ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
