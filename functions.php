15ddd3f5b2e557f0c38dfb430fbdb68c5d07b39a
isakya <1016361268@qq.com>
2024-08-31 21:18:40 +0800

    自定义category分类

--- functions.php
+++ functions.php
@@ -109,10 +109,44 @@ function awesome_custom_post_type (){
             'thumbnail',
             'revisions',
         ),
-        'taxonomies' => array('category', 'post_tag'),
+        // 'taxonomies' => array('category', 'post_tag'),
         'menu_position' => 5,
         'exclude_from_search' => false
     );
     register_post_type('portfolio',$args);
 }
 add_action('init','awesome_custom_post_type');
+
+function awesome_custom_taxonomies() {
+
+    //add new taxonomy hierarchical
+    $labels = array(
+        'name' => 'Types',
+        'singular_name' => 'Type',
+        'search_items' => 'Search Types',
+        'all_items' => 'All Types',
+        'parent_item' => 'Parent Type',
+        'parent_item_colon' => 'Parent Type:',
+        'edit_item' => 'Edit Type',
+        'update_item' => 'Update Type',
+        'add_new_item' => 'Add New Work Type',
+        'new_item_name' => 'New Type Name',
+        'menu_name' => 'Types'
+    );
+
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'type' )
+    );
+
+    register_taxonomy('type', array('portfolio'), $args);
+
+    //add new taxonomy NOT hierarchical
+
+}
+
+add_action( 'init' , 'awesome_custom_taxonomies' );
